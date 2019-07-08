3defa2eb07ae3dd43f2e3ea220f4519252a8303b
Eduardo Iglesias <eriglesias@ufl.edu>
2019-07-08 01:11:17 -0400

    add more seed data including second user and three new tasks

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 
 const userOneId = new mongoose.Types.ObjectId()
@@ -14,9 +15,46 @@ const userOne = {
   }]
 }
 
-const setupDatabase = async () => {
-  await User.deleteMany()
-  await new User(userOne).save()
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+  _id: userTwoId,
+  name: 'John',
+  email: 'john@example.com',
+  password: 'pass123!',
+  tokens: [{
+    token: jwt.sign({_id: userTwoId}, process.env.JWT_SECRET)
+  }]
 }
 
-module.exports = {userOneId, userOne, setupDatabase}
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'First Task',
+  completed: false,
+  owner: userOneId
+}
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Second Task',
+  completed: true,
+  owner: userOneId
+}
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Third Task',
+  completed: true,
+  owner: userTwoId
+}
+
+const setupDatabase = async () => {
+  await User.deleteMany()
+  await Task.deleteMany()
+  await new User(userOne).save()
+  await new User(userTwo).save()
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
+}
+
+module.exports = {userOneId, userOne, userTwo, taskOne, taskTwo, taskThree, setupDatabase}
